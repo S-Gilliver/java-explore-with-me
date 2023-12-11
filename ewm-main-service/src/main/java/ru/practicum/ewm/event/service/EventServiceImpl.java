c6932e676bc1b3928bb63217e4ed6e8bfaea618b
S-Gilliver <shtyrkov.dmitry@yandex.ru>
2023-12-11 12:23:31 +0300

    add main_svc

--- ewm-main-service/src/main/java/ru/practicum/ewm/event/service/EventServiceImpl.java
+++ ewm-main-service/src/main/java/ru/practicum/ewm/event/service/EventServiceImpl.java
@@ -62,9 +62,7 @@ public class EventServiceImpl implements EventService {
     public final EventRepository eventRepository;
 
     public final CategoryService categoryService;
-    
     public final UserService userService;
-    
     public final LocationRepository locationRepository;
 
     public final ParticipationRequestRepository participationRequestRepository;
